48947807058836f1e16b437682884809dfcf35b5
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-10 15:50:40 +0200

    slug route and params done

--- routes/heroes.js
+++ routes/heroes.js
@@ -5,5 +5,10 @@ const heroesJson = require("../heroes.json");
 app.get("/", (req, res) => {
   res.json(heroesJson);
 });
+app.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+
+  res.send(slug);
+});
 
 module.exports = app;
