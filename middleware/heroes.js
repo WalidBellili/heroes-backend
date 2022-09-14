e1a07f8cbd8a91fdd371115e21021a4a1b2793a0
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-14 09:04:10 +0200

    in the middleware : find on heroes array and findIndex to isolate the slug with the req of params

---
+++ middleware/heroes.js
@@ -0,0 +1,23 @@
+const heroes = require("../heroes.json");
+
+const checkIfExists = (req, res, next) => {
+  const { slug } = req.params;
+
+  const hero = heroes.find((hero) => {
+    return slug === hero.slug;
+  });
+
+  const heroIndex = heroes.findIndex((hero) => {
+    return slug === hero.slug;
+  });
+
+  if (hero) {
+    req.hero = hero;
+    req.heroIndex = heroIndex;
+    next();
+  } else {
+    res.status(404).json("not found");
+  }
+};
+
+module.exports = { checkIfExists };
--- routes/heroesRoute.js
+++ routes/heroesRoute.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
+const heroes = require("../heroes.json");
+const { checkIfExists } = require("../middleware/heroes");
+
+app.get("/", (req, res) => {
+  res.json(heroes);
+});
+app.get("/:slug", checkIfExists, (req, res) => {
+  res.json(req.hero);
+});
 
 module.exports = app;
