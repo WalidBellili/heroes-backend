0b8bd9405ba3406bfdd4f9c597f75928981c87df
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-14 12:52:20 +0200

    post ok with all the body and the name slugyfied...

--- routes/heroesRoute.js
+++ routes/heroesRoute.js
@@ -9,6 +9,19 @@ app.get("/", (req, res) => {
 app.get("/:slug", checkIfExists, (req, res) => {
   res.json(req.hero);
 });
+app.get("/:slug/powers", checkIfExists, (req, res) => {
+  res.json(req.hero.power);
+});
+
+app.post("/", (req, res) => {
+  const hero = {
+    ...req.body,
+    slug: req.body.name.toLowerCase().replaceAll(" ", "-"),
+  };
+  heroes.push(hero);
+  res.json(hero);
+});
+
 app.delete("/:slug", checkIfExists, (req, res) => {
   heroes.slice(req.heroIndex, 1);
   res.status(204).json("hero deleted");
